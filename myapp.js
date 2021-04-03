f6649eb1fcfcd39663c24c891f73b241e50457a0
Vicente <vicentedpsantos@gmail.com>
2021-04-03 14:54:56 -0300

    Add deployed url to code

--- myapp.js
+++ myapp.js
@@ -3,11 +3,12 @@ InboxSDK.loadScript("https://unpkg.com/react-dom@17/umd/react-dom.development.js
 InboxSDK.loadScript("https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js")
 
 const templateIframeId = 'templateIframe';
+const reactAppUrl = 'https://hardcore-tesla-72d935.netlify.app'
 
 InboxSDK.load(2, 'sdk_helpmetype_65569f585b').then(sdk => {
   sdk.Compose.registerComposeViewHandler((composeView) => {
       window.addEventListener('message', (e) => {
-        if (e.origin !== 'http://localhost:3000') return
+        if (e.origin !== reactAppUrl) return
 
         console.log(`Received message: ${e.data}`);
         composeView.insertTextIntoBodyAtCursor(e.data);
@@ -19,7 +20,7 @@ InboxSDK.load(2, 'sdk_helpmetype_65569f585b').then(sdk => {
       iconUrl: 'https://static.thenounproject.com/png/365779-200.png',
       onClick: event => {
         const el = document.createElement("iframe");
-        el.setAttribute("src", "http://localhost:3000");
+        el.setAttribute("src", reactAppUrl);
         el.id = templateIframeId;
         el.style.width = "250px";
         el.style.height = "480px";
